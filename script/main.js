dff1052f55b78075eb4e5f1ce50c4206f71ed2fb
kimsohyoun <thgusl2@naver.com>
2021-09-13 15:31:20 +0900

    무슨무슨 작업이라고 간략하게 표기

--- script/main.js
+++ script/main.js
@@ -1,5 +1,48 @@
 $(function(){
+    ad(); // 광고창
+    modal(); // 모달창
+    movieExplorer();
+    setInterval(mouseScroll, 1500);
+});
+function mouseScroll(){
+    $("#mouse").animate({
+        bottom: "-50"
+    },"swing",function(){
+        $("#mouse").animate({
+            bottom: "-40"
+        });
+    })
+}
+function movieExplorer(){
+    $("input[type=image]").click(function(){        
+        // 항상 false : null, undefined, "", 0, false
+        // 나머지는 true
+        var keyword = $("input[type=text]").val();
+        if(!keyword) {
+            alert("영화명을 입력하셔야 합니다");
+        } else {
+            console.log(keyword);
+            // $("input[type=text]").val("");
+            // 실제로는 location.href="" 특정 url 리다이렉트 해야함.
+            location.href="https://www.megabox.co.kr/movie?searchText="+keyword;
+        }
+    });
+}
+function modal() {    
+
+    $(".favorite").click(function(){
+        $("#modal").show();
+    });
+
+    $("#close_x").click(function(){
+        $("#modal").hide();
+    });
+    $("#close_ok").click(function(){
+        $("#modal").hide();
+    });
+}
+function ad(){
     $(".ad-close").click(function(){
         $(".ad").hide();
-    })
-})
+    });
+}
